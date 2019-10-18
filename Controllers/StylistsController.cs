31e7653d498bdc4fecc0f5d77ef4f9110004fe9a
Erin Deuel <end.gaia@gmail.com>
2019-10-18 11:43:37 -0700

    try to take it a step at a time. Still haven't figured out SQL completely

--- Controllers/StylistsController.cs
+++ Controllers/StylistsController.cs
@@ -4,23 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
-namespace Stylists.Controllers
+namespace HairSalon.Controllers
 {
     public class StylistsController : Controller{
         
         private readonly SalonContext _db;
 
-        public CuisineController(SalonContext db)
+        public StylistsController(SalonContext db)
         {
             _db = db;
         }
+
         [HttpGet]
         public ActionResult Index()
         {
             List<Stylists> model = _db.Stylists.ToList();
             return View(model);
         }
-        [HttpGet]
+          [HttpGet]
         public ActionResult Create()
         {
             return View();
@@ -32,24 +33,24 @@ namespace Stylists.Controllers
         _db.SaveChanges();
         return RedirectToAction("Index");
         }
-        [HttpGet]
+          [HttpGet]
         public ActionResult Show(int id)
         {
         Stylists thisStylist = _db.Stylists.FirstOrDefault( Stylists => Stylists.StylistId == id);
-        // List<Store> storesList = _db.Stores.Where(Stores => Stores.CuisineId == id).ToList();
-        // ViewBag.RelatedStores = storesList;
+        List<Customers> customerList = _db.Customers.Where(Customers => Customers.CustomerId == id).ToList();
+        ViewBag.RelatedCustomers = customerList;
         return View(thisStylist);
         }
-        [HttpGet]
+         [HttpGet]
         public ActionResult Delete(int id)
         {
-        Stylists thisStylist = _db.Stylists.FirstOrDefault( Stylists => Stylists.StylistId == id);
+        Stylists thisStylist = _db.Stylists.FirstOrDefault( Stylists => Stylists.stylistId == id);
         return View(thisStylist);
         }
         [HttpPost, ActionName("Delete")]
         public ActionResult Destroy(int id)
         {
-        Stylists thisStylist = _db.Stylists.FirstOrDefault( Stylists => Stylists.StylistId ==  id);
+        Stylists thisStylist = _db.Stylists.FirstOrDefault( Stylists => Stylists.stylistId ==  id);
         _db.Stylists.Remove(thisStylist);
         _db.SaveChanges();
         return RedirectToAction("Index");
@@ -57,16 +58,15 @@ namespace Stylists.Controllers
         [HttpGet]
         public ActionResult Update(int id)
         {
-        Stylists thisStylist = _db.Stylists.FirstOrDefault( Stylists => Stylists.StylisteId == id);
+        Stylists thisStylist = _db.Stylists.FirstOrDefault( Stylists => Stylists.stylistId == id);
         return View(thisStylist);
         }
-        [HttpPost, ActionName("Update")]
+         [HttpPost, ActionName("Update")]
         public ActionResult Update(Stylists stylist)
         {
         _db.Entry(stylist).State = EntityState.Modified;
         _db.SaveChanges();
         return RedirectToAction("Index");
         }
-
     }
 }
--- Models/stylists.cs
+++ Models/stylists.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using MySql.Data.MySqlClient;
 
 namespace HairSalon.Models
 {
@@ -8,51 +7,9 @@ namespace HairSalon.Models
         public string FirstName {get; set;}
         public string LastName {get; set;}
         public int StylistId {get;}
-
-        public Stylists(string first_name, string last_name)
+          public Stylists()
         {
-            FirstName = first_name;
-            LastName = last_name;
+            this.Customers = new HashSet<Customers>();
         }
-        public static Stylists Find (int SearchId)
-        {
-            Stylists placeholderStylist = new Stylists("placeholder Stylist");
-    return placeholderStylist;
-        }
-        public static void ClearAll()
-        {
-            MySqlConnection conn = DB.Connection();
-            conn.Open();
-            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"DELETE FROM salon;";
-            cmd.ExecuteNonQuery();
-            conn.Close();
-            if (conn != null)
-            {
-               conn.Dispose();
-            }
-        }
-        public static List<Stylists> GetAll()
-    {
-        List<Stylists> allStylists = new List<Stylists> { };
-        MySqlConnection conn = DB.Connection();
-        conn.Open();
-        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-        cmd.CommandText = @"SELECT * FROM salon;";
-        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-        while (rdr.Read())
-        {
-            int StylistId = rdr.GetInt32(0);
-            string StylistName = rdr.GetString(1);
-            Stylists newStylist = new Stylists(StylistsName, StylistId);
-            allStylists.Add(newStylist);
-        }
-        conn.Close();
-        if (conn != null)
-        {
-            conn.Dispose();
-        }
-        return allStylists;
-    }
     }
 }
